e8f875311add727d4ea2642953114f0787ba4d08
Wiecek-K <krystiandwiecek@gmail.com>
2023-04-15 13:45:09 +0200

    added function decodeGenreId

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const filmList = document.querySelector(".filmList");
 const API_KEY = "964358699754c21d74c014b561cf196c";
-console.log("lista", filmList);
 export async function fetchFilmList(name, page = 1) {
 	try {
 		const response = await fetch(
@@ -16,11 +15,10 @@ export async function fetchGenres() {
 	try {
 		const response = await fetch(
 			`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`,
+			//https://api.themoviedb.org/3/genre/movie/list?api_key=964358699754c21d74c014b561cf196c&language=en-US
 		);
 		return response.json();
-	} catch (err) {
-		Notify.failure("ERROR", err);
-	}
+	} catch (err) {}
 }
 function createFilmMarkup({
 	original_title,
@@ -57,3 +55,30 @@ Promise.all([fetchGenres(), fetchFilmList()]).then((r) => {
 	const [genres, films] = r;
 	console.log("all", genres.genres, films.results);
 });
+
+const genres = [
+	{ id: 28, name: "Action" },
+	{ id: 12, name: "Adventure" },
+	{ id: 16, name: "Animation" },
+	{ id: 35, name: "Comedy" },
+	{ id: 80, name: "Crime" },
+	{ id: 99, name: "Documentary" },
+	{ id: 18, name: "Drama" },
+	{ id: 10751, name: "Family" },
+	{ id: 14, name: "Fantasy" },
+	{ id: 36, name: "History" },
+	{ id: 27, name: "Horror" },
+	{ id: 10402, name: "Music" },
+	{ id: 9648, name: "Mystery" },
+	{ id: 10749, name: "Romance" },
+	{ id: 878, name: "Science Fiction" },
+	{ id: 10770, name: "TV Movie" },
+	{ id: 53, name: "Thriller" },
+	{ id: 10752, name: "War" },
+	{ id: 37, name: "Western" },
+];
+
+const decodeGenreId = (id) => {
+	return genres.find((genre) => genre.id === id).name;
+};
+
